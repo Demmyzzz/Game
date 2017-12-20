1afc2d52e8b11f39d238d4f3a026b6328bb558ca
Demmyzzz <33146623+Demmyzzz@users.noreply.github.com>
2017-12-20 14:48:22 +0300

    Add files via upload

--- threadcopy/Main.java
+++ threadcopy/Main.java
@@ -12,7 +12,8 @@ public class Main extends Thread{
         this.outputFile = output;
     }
     public void run() {
-        long time = System.currentTimeMillis();
+        System.out.println("start processing... "+getName()+" "+currentThread());
+
         String s;
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(inputFile))) {
             BufferedReader br = new BufferedReader(new FileReader(outputFile));
@@ -22,24 +23,38 @@ public class Main extends Thread{
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println( System.currentTimeMillis()-time+" millis");//выводит сколько миллисекунд работал поток
-        System.out.println(System.currentTimeMillis()+" Millis");//выводи текущее время в миллисекундах
+        System.out.println("end process "+getName()+ " "+ currentThread());
+        //выводит сколько миллисекунд работал поток
+      //  System.out.println(System.currentTimeMillis()+" Millis");выводи текущее время в миллисекундах
     }
     public static void main(String[] args) {
 
             Main mainF = new Main("src\\test.txt", "src\\text.txt");
             Main mainS = new Main("src\\test2.txt", "src\\text2");
-            mainF.start();
-            mainS.start();
-            //запистили 2 потока
             try{
-                // с помощью метода .join мы сделаем так что бы потоки выполнялись послежовательно тоесть программма
-                // сначало запустит и выполнит все операции с первым потоком а потом уже тольк оприступит к следующему потоку
-                mainF.join();
-                mainS.join();
+                mainF.setName("firstThread");
+                mainS.setName("secondThread");
+                long time = System.currentTimeMillis();
+                mainF.start();
+               mainF.join();
+                System.out.println(System.currentTimeMillis() - time+" millis");
+                long time1 = System.currentTimeMillis();
+               mainS.start();
+               mainS.join();
+                System.out.println(System.currentTimeMillis() - time1+" millis");
+
+
+
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
     }
+
 }
+//запистили 2 потока
+
+
+// с помощью метода .join мы сделаем так что бы потоки выполнялись последовательно тоесть программма
+// сначала запустит и выполнит все операции с первым потоком а потом уже только приступит к следующему потоку
