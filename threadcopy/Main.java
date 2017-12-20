0c44c649b500fccc9f6694261153b1887c02d310
Demmyzzz <33146623+Demmyzzz@users.noreply.github.com>
2017-12-20 13:43:35 +0300

    Add files via upload

--- threadcopy/Main.java
+++ threadcopy/Main.java
@@ -12,6 +12,7 @@ public class Main extends Thread{
         this.outputFile = output;
     }
     public void run() {
+        long time = System.currentTimeMillis();
         String s;
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(inputFile))) {
             BufferedReader br = new BufferedReader(new FileReader(outputFile));
@@ -21,12 +22,24 @@ public class Main extends Thread{
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(System.currentTimeMillis() + " Millis ");
+        System.out.println( System.currentTimeMillis()-time+" millis");//выводит сколько миллисекунд работал поток
+        System.out.println(System.currentTimeMillis()+" Millis");//выводи текущее время в миллисекундах
     }
     public static void main(String[] args) {
-        Main mainF = new Main("src\\test.txt", "src\\text.txt" );
-        Main mainS = new Main("src\\test2.txt","src\\text2" );
-        mainF.start();
-        mainS.start();
+
+            Main mainF = new Main("src\\test.txt", "src\\text.txt");
+            Main mainS = new Main("src\\test2.txt", "src\\text2");
+            mainF.start();
+            mainS.start();
+            //запистили 2 потока
+            try{
+                // с помощью метода .join мы сделаем так что бы потоки выполнялись послежовательно тоесть программма
+                // сначало запустит и выполнит все операции с первым потоком а потом уже тольк оприступит к следующему потоку
+                mainF.join();
+                mainS.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
     }
 }
