4dca7c9a82ad5823b2f7d4c7080c261b75e789ae
Demmyzzz <33146623+Demmyzzz@users.noreply.github.com>
2017-12-22 15:54:47 +0300

    Add files via upload

---
+++ url/Download.java
@@ -0,0 +1,34 @@
+package ru.vkd.url;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class Download extends Thread{
+
+    private URL url;
+    private String strUrl;
+    private String file;
+
+    public Download(URL url, String strUrl, String file) {
+        this.url = url;
+        this.strUrl = strUrl;
+        this.file = file;
+        start();
+    }
+
+    public void run(){
+        try {
+            System.out.println("processed... "+ getName());
+            ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
+            FileOutputStream stream = new FileOutputStream(file);
+            stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+            stream.close();
+            byteChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ url/Main.java
@@ -0,0 +1,111 @@
+package ru.vkd.url;
+
+import java.io.*;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main{
+
+    private static final String IN_FILE_TXT = "src\\inFile.txt";
+    private static final String OUT_FILE_TXT = "src\\outFile.txt";
+    private static final String PATH_TO_MUSIC = "src\\ru";
+    public static final String DATA_URL = "\\s*(?<=data-url\\s?=\\s?\")[^>]*\\/*(?=\")";
+
+    public static void main(String[] args) {
+        String Url;
+        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT))) {
+            while ((Url = inFile.readLine()) != null) {
+                System.out.println("first-step-programm");
+                URL url = new URL(Url);
+                String result ="";
+                
+                /*
+                * PageFlow(поток страницы)
+                * этот метд предназначен для для счиывания из общего потока страницы строчку кода
+                * и сохранения этой строки в переменную result для последующего использования в других методах 
+                * */
+
+                PageFlow(url, result);
+                /*
+                * метод CheckPattern ищет ссылки на скачивание музыки и записывает их
+                * в outFile.txt (OUT_FILE_TXT)
+                * */
+                
+                CheckPattern(url, result);
+
+                /*
+                * метод Transfer передаёт в класс Download
+                * URL url
+                * String strUrl
+                * String file
+                * */
+
+                Transfer();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+/*
+
+ */
+    private static String PageFlow(URL url, String result) {
+        System.out.println("second-step-programm");
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            result = bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+/*
+*
+* */
+    private static void CheckPattern(URL url, String result) {
+        System.out.println("thrid-step-programm");
+        try (BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
+            Pattern email_pattern = Pattern.compile(DATA_URL);
+            Matcher matcher = email_pattern.matcher(PageFlow(url, result));
+            int i = 0;
+
+            while (matcher.find() && i < 2) {
+
+                outFile.write(matcher.group() + "\r\n");
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void Transfer() {
+        System.out.println("fourth-step-programm");
+        String music;
+        int count = 0;
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))){
+            while ((music = musicFile.readLine()) != null) {
+
+                downloadUsingNIO(music, PATH_TO_MUSIC + String.valueOf(count) + ".mp3");
+                count++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void downloadUsingNIO(String strUrl, String file) throws IOException {
+        System.out.println("fifth-step-programm");
+        URL url = new URL(strUrl);
+        Download d = new Download(url, strUrl, file);
+        Download threadNumOne = new Download(url,strUrl,file);
+        Download threadNumTwo = new Download(url,strUrl,file);
+
+        threadNumOne.setName("first");
+        threadNumTwo.setName("second");
+
+    }
+}
+
